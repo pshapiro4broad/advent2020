f8e70f8bf8e102986cf12ff29e8500396872c8a6
Phil Shapiro <pshapiro@broadinstitute.org>
2020-12-19 10:29:17 -0500

    refactor common code

--- src/day18.clj
+++ src/day18.clj
@@ -1,7 +1,6 @@
 (ns day18
   (:require [clojure.string :as str]))
 
-
 (def input
   (->> "src/day18-input.txt"
        slurp
@@ -9,18 +8,15 @@
 
 (defn reduce-exp [exp]
   (let [op (re-find #"(\d+) ([\+|\*]) (\d+)" exp)
-        op-loc (and op (str/index-of exp (first op)))
         paren (re-find #"\(([^\(\)]+)\)" exp)
-        paren-loc (and paren (str/index-of exp (first paren)))]
+        make-reduced
+        (fn [found reduced]
+          (reduce-exp (str/replace-first exp (first found) (str reduced))))]
     (cond
-      paren
-      (reduce-exp (str (subs exp 0 paren-loc) (reduce-exp (nth paren 1))
-                       (subs exp (+ paren-loc (count (first paren))))))
-      op
-      (reduce-exp (str (subs exp 0 op-loc) ((if (= "+" (nth op 2)) + *)
-                                            (read-string (nth op 1))
-                                            (read-string (nth op 3)))
-                       (subs exp (+ op-loc (count (first op))))))
+      paren (make-reduced paren (reduce-exp (nth paren 1)))
+      op (make-reduced op ((if (= "+" (nth op 2)) + *)
+                           (read-string (nth op 1))
+                           (read-string (nth op 3))))
       :else (read-string exp))))
 
 (defn part1 []
@@ -30,23 +26,17 @@
 
 (defn reduce-exp2 [exp]
   (let [plus (re-find #"(\d+) \+ (\d+)" exp)
-        plus-loc (and plus (str/index-of exp (first plus)))
         mult (re-find #"(\d+) \* (\d+)" exp)
-        mult-loc (and mult (str/index-of exp (first mult)))
         paren (re-find #"\(([^\(\)]+)\)" exp)
-        paren-loc (and paren (str/index-of exp (first paren)))]
+        make-reduced
+        (fn [found reduced]
+          (reduce-exp2 (str/replace-first exp (first found) (str reduced))))]
     (cond
-      paren
-      (reduce-exp2 (str (subs exp 0 paren-loc) (reduce-exp2 (nth paren 1))
-                       (subs exp (+ paren-loc (count (first paren))))))
-      plus
-      (reduce-exp2 (str (subs exp 0 plus-loc) (+ (read-string (nth plus 1))
-                                                (read-string (nth plus 2)))
-                       (subs exp (+ plus-loc (count (first plus))))))
-      mult
-      (reduce-exp2 (str (subs exp 0 mult-loc) (* (read-string (nth mult 1))
-                                                (read-string (nth mult 2)))
-                       (subs exp (+ mult-loc (count (first mult))))))
+      paren (make-reduced paren (reduce-exp2 (nth paren 1)))
+      plus (make-reduced plus (+ (read-string (nth plus 1))
+                                 (read-string (nth plus 2))))
+      mult (make-reduced mult (* (read-string (nth mult 1))
+                                 (read-string (nth mult 2))))
       :else (read-string exp))))
 
 (defn part2 []
@@ -54,7 +44,7 @@
        (map reduce-exp2)
        (reduce +)))
 
-(comment
+;(comment
   (println "part 1: " (part1))
   (println "part 2: " (part2))
-  )
+  ;)
