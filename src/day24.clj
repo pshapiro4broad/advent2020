943aa0ea763bd8bcc4222607535d423e67847025
philshap <philshap@gmail.com>
2020-12-24 11:06:54 -0500

    minor improvements

--- src/day24.clj
+++ src/day24.clj
@@ -10,47 +10,36 @@
 (defn parse-line [line]
   (map keyword (re-seq #"e|se|sw|w|nw|ne" line)))
 
-(defn dir->delta [dir]
-  (case dir
-    :e [1 0]
-    :se [0 1]
-    :sw [-1 1]
-    :w [-1 0]
-    :nw [0 -1]
-    :ne [1 -1]))
+(def dir->delta
+  {:e [1 0], :se [0 1], :sw [-1 1], :w [-1 0], :nw [0 -1], :ne [1 -1]})
 
 (defn flip-tile [tiles tile]
   (if (get tiles tile)
     (disj tiles tile)
     (conj tiles tile)))
 
-;; can be optimied, was written before to flip every tile
 (defn load-tiles [input]
-  (loop [tiles #{}
-         lines input]
-    (if (empty? lines)
-      tiles
-      (recur
-        (flip-tile tiles
-                   (loop [deltas (map dir->delta (parse-line (first lines)))
-                          pos [0 0]]
-                     (if (empty? deltas)
-                       pos
-                       (recur (rest deltas) (mapv + (first deltas) pos)))))
-        (rest lines)))))
+  (reduce
+    (fn [tiles line]
+      (flip-tile
+        tiles
+        (reduce (fn [pos delta] (mapv + delta pos))
+                [0 0]
+                (map dir->delta line))))
+    #{}
+    (->> input (map parse-line))))
 
 (defn part1 []
   (count (load-tiles input)))
 
-;; copied from day 17
-(def adjacent
-  [[0 0] [1 0] [0 1] [-1 1] [-1 0] [0 -1] [1 -1]])
+;; copied from day 17, more or less
+(def adjacent (vals dir->delta))
 
 (defn possible-active [tiles]
-  (->> (for [tile tiles
-             near adjacent]
-         (mapv + tile near))
-       distinct))
+  (-> (for [tile tiles
+            near adjacent]
+        (mapv + tile near))
+      distinct))
 
 (defn active-neighbors [all-alive tile]
   (count
@@ -63,7 +52,7 @@
   (let [active? (get tiles tile)
         active-neighbors (active-neighbors tiles tile)]
     (if active?
-      (or (= active-neighbors 2) (= active-neighbors 3)) ; values are +1 because the current tile is counted
+      (or (= active-neighbors 1) (= active-neighbors 2))
       (= active-neighbors 2))))
 
 (defn next-cycle [tiles]
