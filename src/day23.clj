e40b6f2a64089b0401b06f5a44aa15f7d053c034
Phil Shapiro <pshapiro@broadinstitute.org>
2020-12-26 21:11:48 -0500

    implement cups using forward link array

--- src/day23.clj
+++ src/day23.clj
@@ -1,44 +1,7 @@
 (ns day23
-  (:require [clojure.string :as str])
-  (:import (java.util LinkedList)))
+  (:require [clojure.string :as str]))
 
 (def input "364297581")
-
-;; ring
-(defn make-ring [] (LinkedList.))
-(defn r-peek [ring] (.peekFirst ring))
-(defn r-peek-last [ring] (.peekLast ring))
-(defn r-pop [ring] (.pop ring))
-(defn r-pop-last [ring] (.removeLast ring))
-(defn r-push [ring x] (.push ring x))
-(defn r-push-last [ring x] (.addLast ring x))
-(defn r-length [ring] (.size ring))
-(defn r-nth [ring i] (.get ring i))
-
-(defn rotate [ring]
-  (let [first (r-peek ring)]
-    (r-pop ring)
-    (r-push-last ring first)))
-
-(defn rotate-to [ring v]
-  (if (= (r-peek ring) v)
-    ring
-    (do
-      (rotate ring)
-      (rotate-to ring v))))
-
-(defn r-print [ring]
-  (->> (range (r-length ring))
-      (map #(r-nth ring %))
-      (str/join "")))
-
-(defn parse-input [input]
-  (let [ring (make-ring)]
-    (doall
-      (->> (str/split input #"")
-           (map read-string)
-           (map #(r-push-last ring %))))
-    ring))
 
 (defn find-destination [cup skip]
   (loop [cup (if (= cup 1) 9 (dec cup))]
@@ -46,32 +9,63 @@
       (recur (if (= cup 1) 9 (dec cup)))
       cup)))
 
-(defn move [cups]
-  (let [current (r-peek cups)]
-    (rotate cups)
-    (let [c1 (r-pop cups)
-          c2 (r-pop cups)
-          c3 (r-pop cups)
-          destination (find-destination current #{c1 c2 c3})]
-      (rotate-to cups destination)
-      (rotate cups)
-      (r-push cups c3)
-      (r-push cups c2)
-      (r-push cups c1)
-      (rotate-to cups current)
-      (rotate cups))))
+(defn cups-next [cups cup]
+  (nth cups cup))
+
+(defn make-cups [input total]
+  (let [data (->> (str/split input #"")
+                  (map read-string)
+                  (#(concat % (range (inc (count input)) (inc total)))))]
+    (loop [cups (into [] (repeat (count data) 0))
+           index 0]
+      (if (= index (dec total))
+        {:cups (assoc cups (last data) (first data)) :current (first data)}
+        (recur (assoc cups (nth data index) (nth data (inc index)))
+               (inc index))))))
+
+(defn print-cups [{cups :cups current :current}]
+  (loop [next-cup (nth cups current)
+         result [current]]
+    (if (= next-cup current)
+      result
+      (recur (nth cups next-cup) (conj result next-cup)))))
+
+(def test-input "389125467")
+
+(defn move [{cups :cups current :current}]
+  (let [c1 (cups-next cups current)
+        c2 (cups-next cups c1)
+        c3 (cups-next cups c2)
+        cups (assoc cups current (cups-next cups c3))
+        destination (find-destination current #{c1 c2 c3})
+        old-next (cups-next cups destination)
+        cups (assoc cups destination c1)
+        cups (assoc cups c3 old-next)]
+    {:cups cups :current (cups-next cups current)}))
 
 (defn part1 []
-  (let [cups (parse-input input)]
-    (dotimes [_ 100] (move cups))
-    (rotate-to cups 1)
-    (r-pop cups)
-    (r-print cups)))
+  (let [cups (make-cups input (count input))]
+    (loop [times 100
+           cups cups]
+      (if (zero? times)
+        (->> (print-cups {:cups (cups :cups) :current 1})
+            next
+            (apply str))
+        (recur (dec times) (move cups))))))
 
-(defn part2 []
-  )
+(defn part2 [num-cups num-moves]
+  (let [cups (make-cups input num-cups)]
+    (loop [times num-moves
+           cups cups]
+      (if (zero? times)
+        (->> (print-cups {:cups (cups :cups) :current 1})
+             next
+             (take 2)
+             (reduce *))
+        (recur (dec times) (move cups))))))
 
+;(assert (not= (part1) 47382659) "broken")
 ;(comment
   (println "part 1: " (part1))
-  (println "part 2: " (part2))
+  ;(println "part 2: " (part2))
   ;)
